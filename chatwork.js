4bdcc681627bad107e42ff78aba285f3b93972fe
acchanAlexander <AtariYoshihiro@next-group.jp>
2017-01-14 17:22:11 +0900

    chatworkモジュールの追加

---
+++ chatwork.js
@@ -0,0 +1,51 @@
+'use strict'
+const request = require('request');
+
+const chatwork = {
+  init: (initParams) => {
+    chatwork.headers = {
+      'X-ChatWorkToken': initParams.chatworkToken
+    };
+    chatwork.roomId = initParams.roomId;
+    chatwork.msg = initParams.msg;
+  },
+
+  getRoomMessages: (callback) => {
+    const options = {
+      uri: 'https://api.chatwork.com/v1/rooms/' + chatwork.roomId + '/messages',
+      headers: chatwork.headers,
+      json: true
+    };
+
+    request.get(options, (error, response, body) => {
+      if (!error && response.statusCode === 200) {
+        callback(body);
+      } else {
+        console.log('error: '+ response.statusCode);
+      }
+    });
+  },
+
+  postRoomMessages: (callback) => {
+    const options = {
+      uri: 'https://api.chatwork.com/v1/rooms/' + chatwork.roomId + '/messages',
+      form: {
+        body: chatwork.msg
+      },
+      headers: chatwork.headers,
+      json: true
+    };
+
+    request.post(options, (error, response, body) => {
+      if (!error && response.statusCode === 200) {
+        console.log('postRoomMessage');
+        console.log(body);
+        callback(body);
+      } else {
+        console.log('error: '+ response.statusCode);
+      }
+    });
+  }
+}
+
+module.exports = chatwork;
